16c88e23c929422f79c53b1e7917c9edb2f96219
Alexis Parave <aparavev@gmail.com>
2022-07-13 16:52:25 -0500

    CHORE: testing push

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     comp = Company.find_by(email: params[:email])
     if comp&.authenticate(params[:password])
       comp.regenerate_token
-      render json: comp
+      render json: {...comp, message: "test push"  }
     else
       render json: { error: 'Incorrect email or password' }, status: :unauthorized
     end
