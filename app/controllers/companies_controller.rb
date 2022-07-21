c6d76d826d54dd419b45bbbb18adc50d21c2306b
Alexis Parave <aparavev@gmail.com>
2022-07-20 19:01:53 -0500

    test create company with logo

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -5,4 +5,19 @@ class CompaniesController < ApplicationController
     # @companies = Company.all
     # render json: @companies
   end
+
+  def create
+    @company = Company.new(company_params)
+    if @company.save
+      render json: { company: @company, logo_path: url_for(@company.logo) }
+    else
+      render json: { error: @companies.errors.full_messages }
+    end
+  end
+
+  private
+
+  def company_params
+    params.require(:company).permit(:name, :logo)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
 
   get '/companies' => 'companies#index'
+  post '/companies' => 'companies#create'
 end
